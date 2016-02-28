010f52318e8ce8b2bf65a79279271f3ce4eaf7d8
sriramrao1 <sriramrao1@users.noreply.github.com>
2016-02-28 00:18:47 -0500

    Create tblSurveyResults.sql

---
+++ transforming/tblSurveyResults.sql
@@ -0,0 +1,1 @@
+create table tblSurveyResults as Select providerid, hospital, cast(HCAHPSBaseScore as int), cast (HCAHPSConsistencyScore as int), (cast(HCAHPSBaseScore as int)+cast (HCAHPSConsistencyScore as int)) as surveyscore, rank() over (Order by (cast(HCAHPSBaseScore as int)+cast (HCAHPSConsistencyScore as int)) desc) as surveyrank from surveyinfo
