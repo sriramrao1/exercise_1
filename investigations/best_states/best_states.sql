47f18010cdf0ae84c03220b3cb72a92572f9da19
sriramrao1 <sriramrao1@users.noreply.github.com>
2016-02-28 01:52:19 -0500

    Update best_states.sql

--- investigations/best_states/best_states.sql
+++ investigations/best_states/best_states.sql
@@ -1,1 +1,1 @@
-select State, avg(ProcedureNationalRank) as overallrank from tblHospital group by State order by overallrank limit 10;
+select State, avg(ProcedureNationalRank) as overallrank from tblHospitalProcedureScores group by State order by overallrank limit 10;
