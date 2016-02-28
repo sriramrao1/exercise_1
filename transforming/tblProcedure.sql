7ed507a55240af56955aa1042abb99c8672ac7ed
sriramrao1 <sriramrao1@users.noreply.github.com>
2016-02-28 01:40:40 -0500

    Update tblProcedure.sql

--- transforming/tblProcedure.sql
+++ transforming/tblProcedure.sql
@@ -1,1 +1,1 @@
-CREATE TABLE tblProcedure1 AS SELECT ProcedureID, (stddev(ProcedureScore)) as ProcedureScoreStdDev, rank() over (Order by (stddev(ProcedureScore)) desc) as ProcedureScoreRank FROM tblHospitalProcedureScores group by ProcedureID;
+CREATE TABLE tblProcedure AS SELECT ProcedureID, (stddev(ProcedureScore)) as ProcedureScoreStdDev, rank() over (Order by (stddev(ProcedureScore)) desc) as ProcedureScoreRank FROM tblHospitalProcedureScores group by ProcedureID;
