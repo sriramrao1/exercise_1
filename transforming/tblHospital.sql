f111ddeecb613e049ba26e3e40d20f5b7b6ae439
sriramrao1 <sriramrao1@users.noreply.github.com>
2016-02-28 12:23:51 -0500

    Update tblHospital.sql

--- transforming/tblHospital.sql
+++ transforming/tblHospital.sql
@@ -1,1 +1,2 @@
+drop table tblHospital;
 create table tblHospital as select ProviderID, HospitalName, avg(ProcedureNationalRank) as overallrank FROM tblHospitalProcedureScores group by providerid, hospitalname;
