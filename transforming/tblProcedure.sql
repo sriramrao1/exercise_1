f699bbc4f680de8890ef60297796f6f866847836
sriramrao1 <sriramrao1@users.noreply.github.com>
2016-02-28 12:22:50 -0500

    Update tblProcedure.sql

--- transforming/tblProcedure.sql
+++ transforming/tblProcedure.sql
@@ -1,1 +1,2 @@
+drop table tblProcedure;
 CREATE TABLE tblProcedure AS SELECT ProcedureID, (stddev(ProcedureScore)) as ProcedureScoreStdDev, rank() over (Order by (stddev(ProcedureScore)) desc) as ProcedureScoreRank FROM tblHospitalProcedureScores group by ProcedureID;
