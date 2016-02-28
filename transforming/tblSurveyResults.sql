432bd55ce3586880d03cce74565a3597888f2c05
sriramrao1 <sriramrao1@users.noreply.github.com>
2016-02-28 12:23:22 -0500

    Update tblSurveyResults.sql

--- transforming/tblSurveyResults.sql
+++ transforming/tblSurveyResults.sql
@@ -1,1 +1,2 @@
-create table tblSurveyResults as Select providerid, hospital, cast(HCAHPSBaseScore as int), cast (HCAHPSConsistencyScore as int), (cast(HCAHPSBaseScore as int)+cast (HCAHPSConsistencyScore as int)) as surveyscore, rank() over (Order by (cast(HCAHPSBaseScore as int)+cast (HCAHPSConsistencyScore as int)) desc) as surveyrank from surveyinfo
+drop table tblSurveyResults;
+create table tblSurveyResults as Select providerid, hospital, cast(HCAHPSBaseScore as int), cast (HCAHPSConsistencyScore as int), (cast(HCAHPSBaseScore as int)+cast (HCAHPSConsistencyScore as int)) as surveyscore, rank() over (Order by (cast(HCAHPSBaseScore as int)+cast (HCAHPSConsistencyScore as int)) desc) as surveyrank from surveyinfo;
